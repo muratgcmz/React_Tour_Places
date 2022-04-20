35743bc260375bdf58aba7e6b3c58fd976688f37
muratgcmz <gocmezmurat@gmail.com>
2022-04-20 23:43:22 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss"
+import Navbar from "./components/Navbar/Navbar"
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome React</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return(
+        <div className= "navbar">
+            <a href="#"></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+                
+
+        </div>
+
+    )
+}
+export default Navbar
